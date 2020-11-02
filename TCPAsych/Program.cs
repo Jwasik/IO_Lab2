404c4fa04c6471b03f0d8d42e6a125264101c8e1
Jędrzej <34661811+Jwasik@users.noreply.github.com>
2020-11-02 19:05:24 +0100

    aaaa

---
+++ TCPAsych/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TCPAsych
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var ip = System.Net.IPAddress.Parse("192.168.0.120");
+            TCPAsynchClasses.Asynch server = new TCPAsynchClasses.Asynch(ip,8888);
+            server.Start();
+        }
+    }
+}
